c6f00c2de98cc5c1663676323c0dcec7c0330b97
Haggai Liu <heng_liu94@hotmail.com>
2017-10-01 16:53:26 -0700

    changed the airconditioned code

--- airconditioned/airconditioned.cpp
+++ airconditioned/airconditioned.cpp
@@ -7,6 +7,7 @@
 #include <utility>
 #include <bitset>
 #include <iterator>
+#include <list>
 
 using namespace std;
 
@@ -21,33 +22,68 @@ bool in_interval(int val ,pair<int,int> itv) {
 
 
 
-void simplify(int * n, set< pair<int,int> > minions) {
+set< pair<int,int> > simplify( set< pair<int,int> > minions) {
 	set< pair<int,int> > :: iterator it1,it2,tmp;
-	bool found;
+	vector<bool> found (minions.size());
+	int i,j;
 
-	for (it1 = minions.begin(); it1!=minions.end();) {
-		found = false;
-		for (it2 = minions.begin(); it2!=minions.end(); ++it2) {
-			if (it1!=it2) {
-				if (in_interval(it1->first,*it2) && in_interval(it1->second,*it2)) {
-					tmp = it1;
-					++tmp;
-					minions.erase(it1);
-					it1= tmp;
-					found = true;
-					(*n)--;
-					break;
-				}
+	// set<list<pair<int,int> >:: iterator > toerase;
+
+	vector< pair<int,int> > minions_copy (minions.size());
+	copy(minions.begin(),minions.end(),minions_copy.begin());
+	minions.clear();
+
+	
+
+	for (i=0;i<minions_copy.size();i++) {
+		found[i] =false;
+		for (j=0;j<minions_copy.size();j++) {
+			if (i!=j && in_interval(
+				minions_copy[j].first,minions_copy[i]) && in_interval(
+				minions_copy[j].second,minions_copy[i]) ) {
+				found[i] = true;
+				break;
 			}
 		}
-		if (!found) {
-			++it1;
+		if (!found[i]) {
+			minions.insert(minions_copy[i]);
 		}
-	} 
+	}
+
+
+	
+
+	// cout << minions.size() <<endl;
+
+	// for (j=0; j<found.size();j++) {
+	// 	cout << j << " " << found[j] << endl;
+	// }
+
+	// for (i=0;i<*n;i++) {
+	// 	if (!found[i]) {
+	// 		// cout << "i=" << i <<endl;
+	// 		minions.push_back(minions_copy[i]);
+	// 	}
+	// }
+
+	// i=0;
+
+	// for (it1=minions.begin();it1!=minions.end();i++) {
+	// 	if (found[i]) {
+	// 		it1 = minions.erase(it1);
+	// 	}else {
+	// 		++it1;
+	// 	}
+	// }
+
+	// *n = minions.size();
+
+	return minions;
+
 }
 
 
-int minrooms(int start, int end, vector< pair<int,int> > minions, int count) {
+int minrooms(int start, int end, vector< pair<int,int> > minions, int count, int endpts[][2]) {
 	if (start>end) {
 		// all minions should be satisfied
 		// for (int i= 0; i<minions.size();i++) {
@@ -56,25 +92,38 @@ int minrooms(int start, int end, vector< pair<int,int> > minions, int count) {
 		return minions_pos.count() == minions.size()? count : -1;
 	}
 
+	if (minions_pos.count()==minions.size()) {
+		return count;
+	}
+
 	// don't use start temperature
-	int ans0 = minrooms(start+1, end, minions, count);
+	int ans0 = minrooms(start+1, end, minions, count, endpts);
+
+	if (endpts[start][0]<0) {
+		return ans0;
+	}
 
 	// now use it
 	int ans1=0;
-	vector<int> new_accom;
-	for (int i =0;i<minions.size();i++) {
-		if (!minions_pos.test(i) && in_interval(start, minions[i])) {
-			new_accom.push_back(i);
-			minions_pos.set(i);
-		}
+
+	int right = endpts[start][1];
+
+	int left = endpts[start][0];
+
+	while (left<=right && minions_pos.test(left)) {left++;}
+	if (right<left) {
+		return ans0;
 	}
 
-	ans1 = minrooms(start+1,end, minions,count+1);
-
-	for (vector<int>::iterator it = new_accom.begin();it!=new_accom.end(); ++it ) {
-		minions_pos.set(*it, 0);
+	for (int i =left;i<=right;i++) {
+		minions_pos.set(i);
 	}
 
+	ans1 = minrooms(start+1,end, minions,count+1, endpts);
+
+	for (int i =left;i<=right;i++) {
+		minions_pos.set(i,0);
+	}
 
 	if (ans0<0) {
 		return ans1;
@@ -87,7 +136,12 @@ int minrooms(int start, int end, vector< pair<int,int> > minions, int count) {
 	return min(ans0,ans1);
 }
 
-
+bool compare(pair<int,int> a,pair<int,int> b) {
+	if (a.first!=b.first) {
+		return b.first>a.first;
+	}
+	return b.second>a.second;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -110,12 +164,38 @@ int main(int argc, char const *argv[])
 		// cout << interval.first << interval.second << endl;
 	}
 
-	simplify(&n, minions);
+	minions =simplify( minions);
+
+	n = minions.size();
+
+	// cout << n << endl;
+
+	// for(set<pair<int,int> >::iterator it = minions.begin();it!=minions.end();++it) {
+	// 	cout << it->first << " " << it->second << endl;
+	// }
 
 	vector< pair<int,int> > minion_vec(n);
 
 	copy(minions.begin(),minions.end(),minion_vec.begin());
-	cout << minrooms(1,2*m,minion_vec, 0)<<endl;
+	sort(minion_vec.begin(),minion_vec.end(),compare);
+
+	int minion_endpts [201][2];
+	for (int i =1; i<=2*m;i++) {
+		minion_endpts[i][0] = -1;
+		minion_endpts[i][1] = -1;
+	}
+
+	for (int j =0;j<n;j++) {
+		for (int i=minion_vec[j].first;i<=minion_vec[j].second;i++) {
+			if (minion_endpts[i][0]<0) {
+				minion_endpts[i][0] = j;
+			}
+			minion_endpts[i][1]=j;
+		}
+	}
+
+
+	cout << minrooms(1,2*m,minion_vec, 0, minion_endpts)<<endl;
 
 	return 0;
 }
